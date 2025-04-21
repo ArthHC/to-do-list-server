682e5292fd58fadc7bc27bd997b9344a17888732
Arthur H Cibok <ahcibok@gmail.com>
2025-04-21 17:25:47 -0300

    Adicionando a possibilidade de criar uma nova task

---
+++ src/to-do-list/to-do.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { ToDoService } from './to-do.service';
+import { Response } from 'express';
+
+@Controller()
+export class ToDoController {
+  constructor(private readonly toDoService: ToDoService) {}
+
+  @Post('create')
+  async createTask(@Body() { title }: { title: string }, @Res() res: Response): Promise<any> {
+    try {
+      const result = await this.toDoService.addToDo(title);
+      return res.json({
+        ...result,
+        success: true,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        message: 'Erro ao criar a tarefa',
+        error: error.message,
+      });
+    }
+  }
+}
---
+++ src/to-do-list/to-do.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ToDoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async addToDo(title: string) {
+    const task = await this.prisma.to_do_list.create({
+      data: {
+        title,
+      },
+    });
+
+    return { message: 'To-do item added', task };
+  }
+}
