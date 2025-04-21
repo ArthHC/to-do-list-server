cb6accd7c4351b4a0c8be051e919459d2138b685
Arthur H Cibok <ahcibok@gmail.com>
2025-04-21 17:54:59 -0300

    Adicionando uma opção de get

--- src/to-do-list/to-do.controller.ts
+++ src/to-do-list/to-do.controller.ts
@@ -1,10 +1,32 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { ToDoService } from './to-do.service';
 import { Response } from 'express';
 
 @Controller('todos')
 export class ToDoController {
   constructor(private readonly toDoService: ToDoService) {}
+
+  // @Get()
+  // async getAllTasks(@Res() res: Response): Promise<any> {
+  //   try {
+  //     const tasks = await this.toDoService.getAllTasks();
+  //     return res.json({
+  //       tasks,
+  //       success: true,
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //     return res.status(500).json({
+  //       message: 'Erro ao buscar as tarefas',
+  //       error: error.message,
+  //     });
+  //   }
+  // }
+
+  @Get()
+  getHello(): string {
+    return 'Lista de To-Dos';
+  }
 
   @Post()
   async createTask(@Body() { title }: { title: string }, @Res() res: Response): Promise<any> {
