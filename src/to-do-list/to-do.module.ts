40d0bb22f60b41718ce830391f5f98f7881fbad4
Arthur H Cibok <ahcibok@gmail.com>
2025-04-21 18:23:18 -0300

    Removendo uma vírgula do to-do modules

--- src/to-do-list/to-do.module.ts
+++ src/to-do-list/to-do.module.ts
@@ -6,6 +6,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   controllers: [ToDoController],
-  providers: [ToDoService,],
+  providers: [ToDoService],
 })
 export class ToDoModule {}
